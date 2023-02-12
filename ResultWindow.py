964dae17b72749daa3955c086a5a0b8e919c046b
Marek Dachniewicz <marek138@o2.pl>
2023-02-12 23:22:39 +0100

    feat: add States class, undo and redo methods

--- ResultWindow.py
+++ ResultWindow.py
@@ -15,26 +15,77 @@ from PyQt5 import QtCore
 
 
 class ResultWindow(QMainWindow):
-
+    class States:
+        def __init__(self, data):
+            self.states=[data]
+            self.activeState = 0
+            
+        def undoPossible(self):
+            if self.activeState > 0:
+                return True
+            else:
+                return False
+            
+        def redoPossible(self):
+            if self.activeState < len(self.states):
+                return True
+            else:
+                return False
+            
+        def newState(self, data):
+            if self.redoPossible():
+                del self.states[self.activeState+1:]
+            self.states.append(data)
+            
+                
+            if len(self.states)>5:
+                self.states.pop(0)
+            self.activeState=len(self.states)
+            
+        def prevState(self):
+            if self.undoPossible():
+                self.activeState-=1
+                return self.states[self.activeState]
+            
+        def nextState(self):
+            if self.redoPossible():
+                self.activeState+=1
+                return self.states[self.activeState]
+            
 
     def __init__(self, data=None, parent=None, width=5, height=4, dpi=100, name=None):
         
         self.parent=parent
         self.data=data
+        self.winState = self.States(data)
+        
               
         super(ResultWindow, self).__init__()
         
         # Creating figure canvas
-        sc = FigureCanvas(self, width=width, height=height, dpi=dpi)
+        self.sc = FigureCanvas(self, width=width, height=height, dpi=dpi)
         # Ploting image in axes
-        data.plotData(sc.axes)
+        self.data.plotData(self.sc.axes)
         
-        self.setCentralWidget(sc)
-        self.show()
+        self.setCentralWidget(self.sc)
+        #self.show()
         
         # Setting windown name
         if name==None:
             self.setWindowTitle(data.filename)
+            
+    def drawPlot(self):
+        self.data.plotData(self.sc.axes)
+        
+    def undo(self):
+        self.data=self.winState.prevState()
+        
+    def redo(self):
+        self.data=self.winState.nextState()
+    
+    def modifyData(self, method):
+        self.data=method()
+        self.winState.newState(self.data)
         
         
     # Event handling    
@@ -53,13 +104,14 @@ class TopographyWindow(ResultWindow):
     def __init__(self, data=None, parent=None, width=5, height=4, dpi=100, name=None):
         
         super().__init__(data, parent, width, height, dpi, name)
-        
+        self.show()
 
     
 class SpectroscopyWindow(ResultWindow):
     def __init__(self, data=None, parent=None, width=5, height=4, dpi=100, name=None):
         
         super().__init__(data, parent, width, height, dpi, name)
+        self.show()
     
 class FigureCanvas(FigureCanvasQTAgg):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
