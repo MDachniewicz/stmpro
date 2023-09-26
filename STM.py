af552383bcac47f10be55f52dc9e540803b50209
Marek Dachniewicz <marek138@o2.pl>
2023-09-26 18:40:20 +0200

    chore: cleanup

--- STM.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-import STMgui
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    win = STMgui.MainWindow()
-    win.show()
-    sys.exit(app.exec())
